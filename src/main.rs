0e387be3319831a81f8d873f18e6fd1a6f2fe4e8
hakunekooo <hakunekooo@proton.me>
2023-06-24 07:08:47 +0600

    add rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello World");
+    println!("Guess The Number!");
+    
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+        println!("You've guessed {guess}");
+
 }
